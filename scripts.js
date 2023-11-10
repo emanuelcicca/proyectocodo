fa52bb4d9b4fb1ae67bef6560921ba3eb90a7525
emanuel <emanuelciccarone@gmail.com>
2023-11-10 04:07:06 -0300

    mas de js

--- scripts.js
+++ scripts.js
@@ -1,32 +1,63 @@
-const ticket = 200;
+document.addEventListener('DOMContentLoaded', function () {
+    const ticket = 200;
+    let descEstudiante = 0.80;
+    let descTrainee = 0.50;
+    let descJunior = 0.15;
 
-let descEstudiante = 0.80;
-let descTrainee = 0.50;
-let descJunior = 0.15;
+    const cantidad = document.getElementById('cantidad');
+    const categoria = document.getElementById('categoria');
+    const totalAPagar = document.getElementById('totalAPagar');
+    const btnResumen = document.getElementById('resumen');
+    const cambiarFondoBtn = document.getElementById('cambiarFondoBtn');
 
-const cantidad = document.getElementById('cantidad');
-const categoria = document.getElementById('categoria');
-const totalpagar = document.getElementById('totalAPagar');
-const btnResumen = document.getElementById('resumen');
+    console.log(cantidad, categoria, totalAPagar, btnResumen);
 
-function calcularPago() {
-    let total = cantidad.value * ticket;
-    switch (categoria.value) {
-        case "estudiante":
-            total = total - (total * descEstudiante);
-            break;
-        case "trainee":
-            total = total - (total * descTrainee);
-            break;
-        case "junior":
-            total = total - (total * descJunior);
-            break;
+    btnResumen.addEventListener('click', (evento) => {
+        evento.preventDefault();
+        validarFormulario();
+    });
+
+    const imagenesFondo = [
+        'url("imagenes/cataratas.jpg")',
+        'url("imagenes/paris.jpg")',
+        'url("imagenes/machu pichu.jpg")'
+    ];
+
+    let indiceFondo = 0;
+
+    cambiarFondoBtn.addEventListener('click', function () {
+        document.body.style.backgroundImage = imagenesFondo[indiceFondo];
+        indiceFondo = (indiceFondo + 1) % imagenesFondo.length;
+    });
+
+    function calcularPago() {
+        let total = cantidad.value * ticket;
+        switch (categoria.value) {
+            case "estudiante":
+                total = total - (total * descEstudiante);
+                break;
+            case "trainee":
+                total = total - (total * descTrainee);
+                break;
+            case "junior":
+                total = total - (total * descJunior);
+                break;
+        }
+
+        totalAPagar.textContent = `Total a Pagar: $ ${total}`;
     }
 
-    totalAPagar.textContent = `Total a Pagar: $ ${total}`;
-}
+    function validarFormulario() {
+        var nombre = document.querySelector('#formulario input[placeholder="Nombre"]').value;
+        var apellido = document.querySelector('#formulario input[placeholder="Apellido"]').value;
+        var correo = document.querySelector('#formulario input[placeholder="Correo"]').value;
+        var cantidadValue = parseInt(cantidad.value);
 
-btnResumen.addEventListener('click', (evento) => {
-    evento.preventDefault();
-    calcularPago();
+        if (nombre === '' || apellido === '' || correo === '' || isNaN(cantidadValue) || cantidadValue <= 0 || cantidadValue.toString() !== cantidad.value) {
+            alert('Rellenar todos los campos correctamente y asegurarse de que la cantidad sea un número entero positivo.');
+        } else {
+            calcularPago();
+        }
+    }
 });
+
